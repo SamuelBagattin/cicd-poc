c8d76f117fd2814544ec9bde0a72b74eb83f8087
samuel <samuel.bagattin@gmail.com>
2020-02-05 22:26:52 +0100

    fix HealthController.cs

--- MusicStore/Controllers/HealthController.cs
+++ MusicStore/Controllers/HealthController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace MusicStore.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("")]
     public class HealthController : ControllerBase
     {
 
