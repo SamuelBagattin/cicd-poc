938c01cd1e842fbdf96278a2fab5f37a564c1409
samuel <samuel.bagattin@gmail.com>
2020-02-05 22:15:56 +0100

    add healthcheck route

---
+++ MusicStore/Controllers/HealthController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MusicStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public HealthController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult HealthCheck()
+        {
+            return Ok();
+        }
+    }
+}
